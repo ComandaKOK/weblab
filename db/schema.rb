e2bf7c8bdc6f0e3aa658029cb161f1c527e6a1e7
ComandaKOK <rmkaporin@gmail.com>
2015-09-15 22:26:36 +0300

    Changing to PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150915164733) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feedbacks", force: :cascade do |t|
     t.string   "author"
     t.string   "email"
