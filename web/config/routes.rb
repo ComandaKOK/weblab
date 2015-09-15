2ced54fbf30601bb33df53e94fd403a188d74b43
ComandaKOK <rmkaporin@gmail.com>
2015-09-15 09:56:22 +0300

    New + show added

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :posts
 
+  root 'posts#new'
+
+  match '/addnews',      to: 'posts#new',    via: 'get'
+  match '/allnews',      to: 'posts#index',  via: 'get'    
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
