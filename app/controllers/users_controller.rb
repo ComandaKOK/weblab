2a5d10ad3ce6e6e3547d32e5436300cfd15ca1c9
ComandaKOK <rmkaporin@gmail.com>
2015-11-04 21:20:50 +0200

    Fixed  little things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,18 +52,18 @@ class UsersController < ApplicationController
 #    redirect_to users_url
   end
 
-private
-
-  def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  end
-
   def signed_in_user
     unless signed_in?
       store_location
       redirect_to signin_url
     end
+  end
+
+private
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 
   def correct_user
